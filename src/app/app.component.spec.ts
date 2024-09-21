0737713de481a9b9d07a596f693101d516ca8f77
_Arleson_ <146764585+arlesonbaracho@users.noreply.github.com>
2024-09-21 01:35:30 -0300

    Feat: Completo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Leonisia' title`, () => {
+  it(`should have the 'pj-intehgrador' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Leonisia');
+    expect(app.title).toEqual('pj-intehgrador');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Leonisia');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pj-intehgrador');
   });
 });
